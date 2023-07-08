389a4fb6e9e3f1d3e13c67b555917eafea75966d
MR9812 <77191017+MR9812@users.noreply.github.com>
2023-07-08 23:21:30 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
 #group whiten
 #python main.py --dataname cora --epochs 100 --dfr 0.1 --der 0.6 --lr1 5e-4 --lr2 1e-2 --wd2 1e-4 --num_groups 32 --use_norm True --gpu 1
-#84.48+0.81
 #python main.py --dataname citeseer --epoch 50 --n_layers 1 --dfr 0.4 --der 0.6 --lr2 5e-2 --wd2 1e-2 --num_groups 16 --hid_dim 1024 --out_dim 1024 --gpu 1
-#74.28+0.64
 #python main.py --dataname pubmed --epochs 300 --dfr 0.3 --der 0.5 --lr2 1e-2 --wd2 1e-4 --num_groups 32 --gpu 1
-#81.86+0.50
