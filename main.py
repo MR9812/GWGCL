f3246bcde45e794836ee93cd902826d5e32c3273
MR9812 <77191017+MR9812@users.noreply.github.com>
2023-07-08 23:20:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-#python main.py --dataname cora --epochs 100 --dfr 0.1 --der 0.6 --lr1 5e-4 --lr2 1e-2 --wd2 1e-4 --num_groups 32 --gpu 1
-#python main.py --dataname citeseer --epoch 50 --n_layers 1 --dfr 0.1 --der 0.4 --lr2 5e-2 --wd2 1e-2 --num_groups 16 --hid_dim 1024 --out_dim 1024 --gpu 1
-#python main.py --dataname pubmed --epochs 300 --dfr 0.3 --der 0.5 --lr2 1e-2 --wd2 1e-4 --num_groups 32 --gpu 1
 import argparse
 from model import GWGRL, LogReg
 from aug import random_aug
